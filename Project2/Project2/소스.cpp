ee9791ec93ad41c652ef6387322bbd0189be69af
Mad-Alchemist <105469951+Mad-Alchemist@users.noreply.github.com>
2022-06-12 22:01:42 +0900

    VS 잘 모르겠어요

--- Project2/Project2/소스.cpp
+++ Project2/Project2/소스.cpp
@@ -3,8 +3,27 @@
 int main(void)
 {
 	int a[5];
+	int tmp;
 	for (int i = 0; i < 5; i++)
 	{
-		scanf_s("%d", a[i]);
+		scanf_s("%d", &a[i]);
 	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (a[i] < a[j]) {
+
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		printf("%d", a[i]);
+	}
+
+	return 0;
 }
